2fd76640711322f6c4e810f88877a33a0780a81a
arteeeemt <128076184+arteeeemt@users.noreply.github.com>
2023-08-21 09:01:36 +0000

    Deploying to gh-pages from @ arteeeemt/goit-js-hw-09@1ac6eafc81ded41dc67d3ee1eab84fcd59b9809e 🚀

---
+++ 01-color-switcher.324ec3b9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1;var d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!1}))}));e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.324ec3b9.js.map
